a30256bb594358d9b7bfa3c36dd999b844e386b0
tituspijean <tituspijean@outlook.com>
2022-02-25 23:25:03 +0100

    Fix PHP

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,6 +9,8 @@ YNH_PHP_VERSION="7.4"
 # Composer version
 YNH_COMPOSER_VERSION="2.1.3"
 
+pkg_dependencies="php${YNH_PHP_VERSION} php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml"
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
